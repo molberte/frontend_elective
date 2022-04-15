1c88d7c6b8782eeceb474faf6af700b129580e73
KsEv13 <k.evdokimova@innopolis.university>
2022-04-15 16:56:34 +0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-/* ...*/
-import './styles.css'
-import IMAGE from './heart.png'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 //module image + add to declarations
-import { ClickCounter } from './ClickCounter'
+import './App.css'
+
+import { About } from './pages/About'
+import { Contact } from './pages/Contact'
+import { Home } from './pages/Home'
+import { Quiz } from './pages/Quiz'
 
 export const App = () => {
   return (
-    <>
-      <h1>
-        ReactNew - {process.env.NODE_ENV} {process.env.name}{' '}
-      </h1>
-      <img src={IMAGE} alt="Heart Logo" width="300" height="300" />
-      <ClickCounter />
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/quiz" element={<Quiz />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
+
+export default App
---
+++ src/pages/Quiz.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Quiz = () => {
+  return <div className="Quiz"></div>
+}
+
+export default Quiz
