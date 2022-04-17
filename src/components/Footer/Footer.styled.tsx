faf0bdcd1b54a0efbf5e59033046f75ae52371c4
KsEv13 <k.evdokimova@innopolis.university>
2022-04-17 19:02:18 +0300

    gallery

--- src/components/Footer/Footer.styled.tsx
+++ src/components/Footer/Footer.styled.tsx
@@ -3,8 +3,8 @@ import styled from 'styled-components'
 export const FooterStyled = styled.div`
   width: 100%;
   height: 50px;
-  background-color: rgba(36, 23, 44, 1);
-  color: rgb(220, 200, 200);
+  background-color: rgba(50, 37, 60, 1);
+  color: rgb(240, 220, 220);
   position: fixed;
   bottom: 0;
   left: 0px;
---
+++ src/components/Gallery/Gallery.tsx
@@ -0,0 +1,38 @@
+import PhotoAlbum from 'react-photo-album'
+import React from 'react'
+import { GalleryStyled } from './Gallery.styled'
+
+import Priest from '../../assets/Crushes/Priest.jpg'
+import Tadashi from '../../assets/Crushes/Tadashi.jpg'
+import Joe from '../../assets/Crushes/Joe.jpg'
+import Nick from '../../assets/Crushes/Nick.png'
+
+export const Footer = () => {
+  const photos = [
+    {
+      src: Priest,
+      width: 600,
+      height: 900,
+    },
+    {
+      src: Tadashi,
+      width: 600,
+      height: 528,
+    },
+    {
+      src: Joe,
+      width: 1200,
+      height: 675,
+    },
+    {
+      src: Nick,
+      width: 787,
+      height: 799,
+    },
+  ]
+  return (
+    <GalleryStyled>
+      <PhotoAlbum layout="masonry" photos={photos} />
+    </GalleryStyled>
+  )
+}
